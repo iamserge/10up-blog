82cd9bb097de55eef86e0dc3ae9973e7dfb33c74
sradkevics <sradkevics@wundermancommerce.com>
2020-11-03 22:15:47 +0000

    Minor fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,42 +1,41 @@
-import App from "next/app";
 import Head from "next/head";
 import NProgress from 'nprogress';
 import Router from 'next/router';
 import { GlobalProvider } from "providers/global";
 import "../styles/main.scss";
 
-Router.onRouteChangeStart = () => {
-  NProgress.start();
-};
 
-Router.onRouteChangeComplete = () => {
-  NProgress.done();
-};
+export default function MyApp({Component, pageProps}) {
+    
+    Router.events.on('routeChangeStart', () => {
+        NProgress.start();
+    });
 
-Router.onRouteChangeError = () => {
-  NProgress.done();
-};
+    Router.events.on('routeChangeComplete', () => {
+        NProgress.done();
+    });
 
-export default class MyApp extends App {
-    render() {
-        const { Component, pageProps } = this.props;
+    Router.events.on('routeChangeError', () => {
+        NProgress.done();
+    });
 
-        return (
-            <>
-                <Head>
-                    <title>10up Blog</title>
-                    <meta charSet="utf-8" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                    <meta httpEquiv="cleartype" content="on" />
-                    <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
-                    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet"></link>
-                </Head>
-                <GlobalProvider>
-                    <Component {...pageProps} />
-                </GlobalProvider>
-                
-            </>
-        );
-    }
+
+
+    return (
+        <>
+            <Head>
+                <title>10up Blog</title>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta httpEquiv="cleartype" content="on" />
+                <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
+                <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet"></link>
+            </Head>
+            <GlobalProvider>
+                <Component {...pageProps} />
+            </GlobalProvider>
+            
+        </>
+    );
 }
 
